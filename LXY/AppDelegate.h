e024c6614661160e868085c915a3f8188ebff8f3
tuziyuxin <liangxinyu2008@163.com>
2016-10-10 13:08:53 +0800

    Initial Commit

---
+++ LXY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXY
+//
+//  Created by Xinyu Liang on 16/10/10.
+//  Copyright © 2016年 Xinyu Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
